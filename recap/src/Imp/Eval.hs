e60790fcdc5842f54a0a6a0fc7b684faae6ef014
Mikhail Rodionychev <tralllgm@gmail.com>
2024-09-18 23:41:50 +0200

    hw 01 done

--- recap/src/Imp/Eval.hs
+++ recap/src/Imp/Eval.hs
@@ -18,13 +18,58 @@ data Error = UndefinedVar String | ParsingErr String | DivByZero deriving (Show)
 type EvalM = StateT VarMap (ExceptT Error IO)
 
 evalExpr :: Expr -> EvalM Int
-evalExpr = undefined 
+evalExpr (Var variable) = do
+  myMap <- get
+  let value = M.lookup variable myMap
+  case value of
+    Just value -> return value
+    Nothing -> lift $ throwE (UndefinedVar variable)
+
+evalExpr (Const int) = return int
+
+evalExpr (BinOp op lhs rhs) = do
+  lhsResult <- evalExpr lhs
+  rhsResult <- evalExpr rhs
+  case op of
+    Plus -> return $ lhsResult + rhsResult
+    Minus -> return $ lhsResult - rhsResult
+    Mult -> return $ lhsResult * rhsResult
+    Div -> case rhsResult of
+      0 -> lift $ throwE DivByZero
+      _ -> return $ div lhsResult rhsResult
+
 
 evalCom :: Com -> EvalM ()
-evalCom = undefined 
+evalCom (Assign var expr) = do
+  result <- evalExpr expr
+  modify $ M.insert var result
+
+evalCom (Read var) = do
+  value <- lift $ lift getLine
+  case readMaybe value of
+    Just val -> modify $ M.insert var val
+    Nothing -> lift $ throwE (ParsingErr value)
+
+evalCom (Write expr) = do
+  result <- evalExpr expr
+  lift $ lift $ print result
+
+evalCom (Seq comL comR) = do
+  evalCom comL
+  evalCom comR
+
+evalCom (If expr comTrue comFalse) = do
+  result <- evalExpr expr
+  case result of
+    0 -> evalCom comTrue
+    _ -> evalCom comFalse
+
+evalCom Skip = return ()
 
 evalProg :: Prog -> EvalM Int
-evalProg = undefined
+evalProg (Prog com expr) = do
+  evalCom com
+  evalExpr expr
 
 eval :: Prog -> VarMap -> IO (Either Error Int)
 eval program state = runExceptT (evalStateT (evalProg program) state)
