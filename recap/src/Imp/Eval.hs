bd5545dcc5e5c83aec890146de29e3d111c7a79a
InversionSpaces <inversionspaces@vivaldi.net>
2024-09-22 15:49:34 +0200

    HW01 solved

--- recap/src/Imp/Eval.hs
+++ recap/src/Imp/Eval.hs
@@ -1,12 +1,13 @@
 module Imp.Eval where
 
 import Control.Monad.State
-    ( modify, evalStateT, MonadState(get), MonadTrans(lift), StateT )
+    ( modify, gets, evalStateT, MonadTrans(lift), StateT )
 import Control.Monad.Trans.Except ( runExceptT, throwE, ExceptT )
 import qualified Data.Map.Strict as M
 import Text.Printf (printf)
-import Text.Read (readMaybe)
+import Text.Read (readEither)
 import Imp.Lang
+import Control.Monad (when)
 
 -- Variable mapping; shadowing is allowed
 type VarMap = M.Map String Int
@@ -18,13 +19,38 @@ data Error = UndefinedVar String | ParsingErr String | DivByZero deriving (Show)
 type EvalM = StateT VarMap (ExceptT Error IO)
 
 evalExpr :: Expr -> EvalM Int
-evalExpr = undefined 
+evalExpr (Var x) = do
+  mv <- gets $ M.lookup x
+  lift $ maybe (throwE (UndefinedVar x)) return mv
+evalExpr (Const c) = return c
+evalExpr (BinOp op l r) = do
+  lv <- evalExpr l
+  rv <- evalExpr r
+  when (op == Div && rv == 0) (lift $ throwE DivByZero)
+  return $ case op of
+    Plus -> lv + rv
+    Minus -> lv - rv
+    Mult -> lv * rv
+    Div -> lv `div` rv
 
 evalCom :: Com -> EvalM ()
-evalCom = undefined 
+evalCom (Assign n e) = do
+  v <- evalExpr e
+  modify $ M.insert n v
+evalCom (Read n) = do
+  i <- lift $ lift getLine
+  either (lift . throwE . ParsingErr) (modify . M.insert n) $ readEither i
+evalCom (Write e) = do
+  v <- evalExpr e
+  lift $ lift $ print v
+evalCom (Seq l r) = evalCom l >> evalCom r
+evalCom (If e l r) = do
+  v <- evalExpr e
+  if v == 0 then evalCom l else evalCom r
+evalCom Skip = return ()
 
 evalProg :: Prog -> EvalM Int
-evalProg = undefined
+evalProg (Prog c e) = evalCom c >> evalExpr e
 
 eval :: Prog -> VarMap -> IO (Either Error Int)
 eval program state = runExceptT (evalStateT (evalProg program) state)
