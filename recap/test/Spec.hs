118b856a92ca9b84240e6df2794c28e2ea8c2467
InversionSpaces <inversionspaces@vivaldi.net>
2024-10-10 12:38:42 +0200

    Revert Spec.hs

--- recap/test/Spec.hs
+++ recap/test/Spec.hs
@@ -1,26 +1,2 @@
-import Control.Monad.Except (ExceptT, runExceptT)
-import Control.Monad.State (evalStateT, runStateT)
-import Imp.Exprs
-import Imp.Lang as L
-import Imp.SLang as SL
-import Imp.TLang as TL
-
 main :: IO ()
-main = do
-  texpr <- runExceptT $ TL.compile expr4
-  case texpr of
-    Left err -> print err
-    Right (TL.IntExpr ie) -> do
-      sexpr <- runExceptT $ evalStateT (SL.compile ie (SL.S SL.Z)) (SL.Cons "y" SL.Nil)
-      case sexpr of
-        Left err -> print err
-        Right se -> do
-          res <- runExceptT $ evalStateT (SL.evalExpr (GEQS GEQZ) se) (SL.Cons 5 SL.Nil)
-          print res
-    Right (TL.UnitExpr ue) -> do
-      sexpr <- runExceptT $ evalStateT (SL.compile ue (SL.S SL.Z)) (SL.Cons "y" SL.Nil)
-      case sexpr of
-        Left err -> print err
-        Right se -> do
-          res <- runExceptT $ evalStateT (SL.evalExpr (GEQS GEQZ) se) (SL.Cons 5 SL.Nil)
-          print res
+main = putStrLn "Test suite not yet implemented"
