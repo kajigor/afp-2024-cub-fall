de374d0e5becb7c25ea434925118a1e8811d3f39
InversionSpaces <inversionspaces@vivaldi.net>
2024-11-05 21:58:20 +0100

    Made curry typed

--- src/C1_Curry.hs
+++ src/C1_Curry.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE TemplateHaskell #-}
-module C1_Curry where 
+
+module C1_Curry where
 
 import C2_CurryExercise
 
@@ -9,22 +10,21 @@ f2 (a, b) = a + b
 f2' :: Int -> Int -> Int
 f2' = curry f2
 
-
-
 f3 :: (Int, Int, Int) -> Int
 f3 (a, b, c) = a + b + c
 
--- $(makeCurryUntyped 3)
+$(makeCurryUntyped 3)
 
--- f3 :: Int -> Int -> Int -> Int
--- f3' = curry3 f3
+f3' :: Int -> Int -> Int -> Int
+f3' = curry3 f3
 
--- $(makeCurry 4)
+$(makeCurry 4)
 
 f4 :: (Int, Int, Int, Int) -> Int
 f4 (a, b, c, d) = a + b + c + d
 
--- f4' = curry4 f4
+f4' :: Int -> Int -> Int -> Int -> Int
+f4' = curry4 f4
 
 -- f4'' :: (Int, Int, Int, Int) -> Int
--- f4'' = uncurry4 f'
+-- f4'' = uncurry4 f4'
--- src/C2_CurryExercise.hs
+++ src/C2_CurryExercise.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE TemplateHaskell #-}
-module C2_CurryExercise where 
 
+module C2_CurryExercise where
+
+import Control.Monad
 import Language.Haskell.TH
 
 -- task 1: Implement generalized curry splice (inferred types)
@@ -8,24 +10,37 @@ import Language.Haskell.TH
 curryName :: Int -> Name
 curryName n = mkName $ "curry" ++ show n
 
-makeCurryBody :: Int -> Q Exp
-makeCurryBody n = undefined
+makeCurryBody :: Name -> [Name] -> Q Exp
+makeCurryBody f args = appE (varE f) (tupE (varE <$> args))
 
 makeCurryDec :: Int -> Q Dec
-makeCurryDec n = undefined
+makeCurryDec n = do
+  f <- newName "f"
+  args <- replicateM n (newName "x")
+  funD (curryName n) [clause (varP f : (varP <$> args)) (normalB $ makeCurryBody f args) []]
 
 makeCurryUntyped :: Int -> Q [Dec]
-makeCurryUntyped n = undefined
-
+makeCurryUntyped n = return <$> makeCurryDec n
 
 -- task 2: Add explicity signature to curry
 
 makeCurrySig :: Int -> Q Dec
-makeCurrySig = undefined
+makeCurrySig n = do
+  fRet <- newName "r"
+  fArgs <- replicateM n (newName "a")
+  let fRetType = VarT fRet
+  let fArgTypes = VarT <$> fArgs
+  let fArgTuple = foldl AppT (TupleT n) fArgTypes
+  let fType = AppT (AppT ArrowT fArgTuple) fRetType
+  let retType = foldr (AppT . AppT ArrowT) fRetType fArgTypes
+  let tvs = (`PlainTV` InferredSpec) <$> (fRet : fArgs)
+  let signature = ForallT tvs [] (AppT (AppT ArrowT fType) retType)
+  return $ SigD (curryName n) signature
 
 makeCurry :: Int -> Q [Dec]
-makeCurry n = undefined
-
-
+makeCurry n = do
+  sig <- makeCurrySig n
+  dec <- makeCurryDec n
+  return [sig, dec]
 
 -- (bonus) task 3: Do the same for generalized uncurry
