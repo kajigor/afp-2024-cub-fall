34962ef04e3404ba913311ec0b141d4d480b6c0b
InversionSpaces <inversionspaces@vivaldi.net>
2024-11-12 20:50:27 +0100

    Done Task 2

--- hw/app/TreeFusion.hs
+++ hw/app/TreeFusion.hs
@@ -44,17 +44,59 @@ fusedBinaryTreeFoldrBuildTest tree = foldrBinaryTreeFoldrBuild (\x l r -> (abs (
 
 -- Task 2
 
+data BStream a = forall s. BStream (s -> BStep a s) s
+
+data BStep a s = Branch a s s | Stop
+
+streamMap :: (a -> b) -> BStream a -> BStream b
+streamMap f (BStream g s) = BStream h s
+  where
+    h s' = case g s' of
+      Stop -> Stop
+      Branch x l r -> Branch (f x) l r
+
+streamFilter :: (a -> Bool) -> BStream a -> BStream a
+streamFilter p (BStream g s) = BStream h s
+  where
+    h s' = case g s' of
+      Stop -> Stop
+      Branch x l r -> if p x then Branch x l r else Stop
+
+streamFold :: (a -> b -> b -> b) -> b -> BStream a -> b
+streamFold f z (BStream g s) = go z s
+  where
+    go a s' = case g s' of
+      Stop -> a
+      Branch x l r -> f x (go a l) (go a r)
+
+streamify :: BinaryTree a -> BStream a
+streamify = BStream g
+  where
+    g BinaryLeaf = Stop
+    g (BinaryNode v l r) = Branch v l r
+{-# INLINE [0] streamify #-}
+
+unstreamify :: BStream a -> BinaryTree a
+unstreamify (BStream g s) = case g s of
+  Stop -> BinaryLeaf
+  Branch v l r -> BinaryNode v (unstreamify (BStream g l)) (unstreamify (BStream g r))
+{-# INLINE [0] unstreamify #-}
+
+{-# RULES
+"streamify/unstreamify" forall s. streamify (unstreamify s) = s
+  #-}
+
 mapBinaryTreeStream :: (a -> b) -> BinaryTree a -> BinaryTree b
-mapBinaryTreeStream = undefined
+mapBinaryTreeStream f = unstreamify . streamMap f . streamify
 
 filterBinaryTreeStream :: (a -> Bool) -> BinaryTree a -> BinaryTree a
-filterBinaryTreeStream = undefined
+filterBinaryTreeStream p = unstreamify . streamFilter p . streamify
 
-foldrBinaryTreeStream :: (b -> a -> b) -> b -> BinaryTree a -> b
-foldrBinaryTreeStream = undefined
+foldrBinaryTreeStream :: (a -> b -> b -> b) -> b -> BinaryTree a -> b
+foldrBinaryTreeStream f z = streamFold f z . streamify
 
 fusedBinaryTreeStreamTest :: BinaryTree Int -> Int
-fusedBinaryTreeStreamTest = undefined -- see binaryTreeTest
+fusedBinaryTreeStreamTest tree = foldrBinaryTreeStream (\x l r -> (abs ((abs r + 1) * l) + 1) * x) 1 ((mapBinaryTreeStream (+ 1) . filterBinaryTreeStream (\x -> x `rem` 400 /= 0) . mapBinaryTreeStream sqr) tree)
 
 -- Naive implementations for comparing performance and logic
 
