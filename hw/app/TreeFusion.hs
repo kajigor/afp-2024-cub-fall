b43ccee31fbfe32c8add4a9a0b54bc7ab0177fad
enotvtapke <a.stupnikov05@gmail.com>
2024-11-13 23:27:16 +0100

    Make task 1

--- hw/app/TreeFusion.hs
+++ hw/app/TreeFusion.hs
@@ -8,20 +8,61 @@ data BinaryTree a = BinaryLeaf | BinaryNode a (BinaryTree a) (BinaryTree a)
 
 -- Task 1
 
-buildBinaryTree :: (forall b. (a -> b -> b) -> b -> b) -> BinaryTree a
-buildBinaryTree g = undefined
+-- mapFB ::  (elt -> lst -> lst) -> (a -> elt) -> a -> lst -> lst
+-- {-# INLINE [0] mapFB #-} -- See Note [Inline FB functions] in GHC.Internal.List
+-- mapFB c f = \x ys -> c (f x) ys
+-- mapFB c f = \x ys -> c ((f . g) x) ys
+
+-- map2 f (map2 g xs)
+-- build (\c n -> foldr2 (mapFB c f) n (map2 g xs))
+-- build (\c n -> foldr2 (mapFB c f) n (build (\c n -> foldr2 (mapFB c g) n xs)))
+-- build (\c n -> (\c n -> (foldr2 (mapFB c g) n xs) (mapFB c f) n)
+
+-- build (\c n -> (foldr2 (mapFB (mapFB c f) g) n xs))
+-- build (\c n -> (foldr2 (mapFB c (f . g)) n xs)) |
+-- build (\c n -> (foldr2 (mapFB (\x ys -> c (f x) ys) g) n xs))
+-- build (\c n -> (foldr2 (\x ys -> (\x ys -> c (f x) ys) (g x) ys) n xs))
+-- build (\c n -> (foldr2 (\x ys -> c (f (g x)) ys) n xs))
+-- build (\c n -> (foldr2 (c (f (g n)) xs)))
+-- build (\c n -> (foldr2 (c (f (g n)) xs) n xs))
+
+-- build (\c n -> (foldr2 (mapFB c (f . g)) n xs)) 
+-- build (\c n -> (foldr2 (\x ys -> c ((f . g) x) ys) n xs)) 
+-- build (\c n -> (foldr2 (c ((f . g) n) xs) n xs)) 
+
+{-# RULES
+"build/foldr2" forall f z (g :: forall b. (a -> b -> b -> b) -> b -> b). foldrBinaryTreeFoldrBuild f z (buildBinaryTree g) = g f z
+"filterFB"        forall c p q. filterFB (filterFB c p) q = filterFB c (\x -> q x && p x)
+"mapFB"     forall c f g.       mapFB (mapFB c f) g     = mapFB c (f.g)
+  #-}
+
+{-# INLINE [1] buildBinaryTree #-}
+buildBinaryTree :: (forall b. (a -> b -> b -> b) -> b -> b) -> BinaryTree a
+buildBinaryTree g = g BinaryNode BinaryLeaf
+
+{-# INLINE [0] mapFB #-}
+mapFB :: (b -> l -> l -> l) -> (a -> b) -> a -> l -> l -> l
+mapFB c f = \x l r -> c (f x) l r
 
 mapBinaryTreeFoldrBuild :: (a -> b) -> BinaryTree a -> BinaryTree b
-mapBinaryTreeFoldrBuild = undefined
+mapBinaryTreeFoldrBuild f xs = 
+  buildBinaryTree (\c n -> foldrBinaryTreeFoldrBuild (mapFB c f) n xs)
 
 filterBinaryTreeFoldrBuild :: (a -> Bool) -> BinaryTree a -> BinaryTree a
-filterBinaryTreeFoldrBuild = undefined
+filterBinaryTreeFoldrBuild f xs =  buildBinaryTree (\c n -> foldrBinaryTreeFoldrBuild (filterFB c f) n xs)
 
-foldrBinaryTreeFoldrBuild :: (b -> a -> b) -> b -> BinaryTree a -> b
-foldrBinaryTreeFoldrBuild = undefined
+{-# INLINE [0] filterFB #-}
+filterFB :: (a -> b -> b -> b) -> (a -> Bool) -> a -> b -> b -> b
+filterFB c p x r l | p x       = c x r l
+                   | otherwise = r
+
+{-# INLINE [0] foldrBinaryTreeFoldrBuild #-}
+foldrBinaryTreeFoldrBuild :: (a -> b -> b -> b) -> b -> BinaryTree a -> b
+foldrBinaryTreeFoldrBuild _ z BinaryLeaf = z
+foldrBinaryTreeFoldrBuild f z (BinaryNode val l r) = f val (foldrBinaryTreeFoldrBuild f z l) (foldrBinaryTreeFoldrBuild f z r)
 
 fusedBinaryTreeFoldrBuildTest :: BinaryTree Int -> Int
-fusedBinaryTreeFoldrBuildTest = undefined -- see binaryTreeTest
+fusedBinaryTreeFoldrBuildTest tree = foldrBinaryTreeFoldrBuild (\x l r -> (abs l + 1) * (abs r + 1) * x) 1 ((mapBinaryTreeFoldrBuild (+1) . filterBinaryTreeFoldrBuild (\x -> x `rem` 400 /= 0) . mapBinaryTreeFoldrBuild sqr) tree)
 
 -- Task 2
 
