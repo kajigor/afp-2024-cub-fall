74597f495757af6d7aae713732573186f66f481e
Nikita.Dukin <nikita.dukin@jetbrains.com>
2024-12-11 15:15:55 +0100

    fix(*): add info

--- exam-project/app/Main.hs
+++ exam-project/app/Main.hs
@@ -10,7 +10,8 @@ data TestField = TestField Int String deriving (Show)
 instance FromRow TestField where
   fromRow = TestField <$> field <*> field
 
-
+-- To try it, run the following command into terminal
+-- sqlite3 test.db "CREATE TABLE test (id INTEGER PRIMARY KEY, str text); INSERT INTO test (str) VALUES ('test string');
 main = do
     runQuery "test.db" (createInsertQuery "INSERT INTO test (str) VALUES (?)" (OneEl ("ABRA CADABRA !!!" :: String)))
     runQuery "test.db" (createSelectQuery "SELECT * from test" :: SQLCommand [TestField])
