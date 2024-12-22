1d0dc283f1473b4a4a79c2b31a3d5a5ce7c7fb7d
Mikhail Rodionychev <tralllgm@gmail.com>
2024-12-22 01:09:56 +0100

    typechecked vectors and matrix addition

--- src/lh.hs
+++ src/lh.hs
@@ -1,50 +1,58 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE InstanceSigs #-}
--- {-# OPTIONS_GHC -fplugin=LiquidHaskell #-}
--- {-@ LIQUID "--no-termination" @-}
+{-# OPTIONS_GHC -fplugin=LiquidHaskell #-}
+{-@ LIQUID "--no-termination" @-}
 
-module Mx where
+module Lh where
 
-{-@ type BList N a = {l:[a] | len l == N } @-}
+import LiquidHaskell
+
+{-@ type BList a N = {l:[a] | len l == N } @-}
 {-@ type Nat = {i: Int | i >= 0} @-}
 
-{-@ data Vector a = Vec { n :: Nat, elems :: BList a n} @-}
-data Vector a = Vec { n :: Int, elems :: [a] } deriving (Show)
+{-@ data Vector a = Vec {mLen :: Int, elems :: BList a mLen} @-}
+data Vector a = Vec {mLen :: Int, elems :: [a] } deriving (Show)
+{-@ type VectorN a N = {v:Vector a | mLen v == N} @-}
 
-{-@ type SizedVector N a = {v: Vector a | n v == N} @-}
+{-@ data Matrix a = Mat { mRow :: Nat, mCol :: Nat, rows :: VectorN (VectorN a mCol) mRow} @-}
+data Matrix a = Mat { mRow :: Int, mCol :: Int, rows :: Vector ( Vector a ) } deriving (Show)
 
-{-@ data Matrix a = Mat { h :: Nat, w :: Nat, rows :: SizedVector (SizedVector a w) h} @-}
-data Matrix a = Mat { h :: Int, w :: Int, rows :: Vector ( Vector a ) } deriving (Show)
+{-@ type NEMatrix a = {m:Matrix a | mRow m > 0 && mCol m > 0} @-}
+{-@ type MatrixN a R C = {m:Matrix a | mRow m == R && mCol m == C} @-}
 
-{-@ !!! :: x:Vector a -> {v:Int | 0 <= v && v < n x} -> a @-}
+{-@ !!! :: {x:Vector a | mLen x > 0} -> {v:Int | 0 <= v && v < mLen x} -> a @-}
 (!!!) :: Vector a -> Int -> a
-(!!!) (Vec n xs) i = xs !! i
+(!!!) (Vec _ xs) i = xs !! i
 
-{-@ ind :: m:Matrix a -> {i:Nat | i < h m} -> {j:Nat | j < w m} -> a @-}
+{-@ ind :: m:NEMatrix a
+         -> {i:Int | 0 <= i && i < mRow m} 
+         -> {j:Int | 0 <= j && j < mCol m} 
+         -> a @-}
 ind :: Matrix a -> Int -> Int -> a
-ind (Mat h w xs) i j = (xs !!! i) !!! j
+ind (Mat _ _ xs) i j = (xs !!! i) !!! j
 
-{-@ col :: {m:Matrix a | h m > 0} -> {i:Nat | w m > i} -> {v:Vector a | h m == n v} @-}
-col :: Matrix a -> Int -> Vector a
-col m@(Mat h w xs) ix = Vec h $ go m ix
-    where
-        {-@ go :: {v:Matrix a | h v > 0} -> {i:Nat | w v > i} -> [a] @-}
-        go :: Matrix a -> Int -> [a]
-        go m@(Mat h w xs) colx = ind m 0 colx  : go m colx
+-- {-@ col :: m:NEMatrix a -> {i:Nat | mCol m > i} -> v:VectorN a (mCol m) @-}
+-- col :: Matrix a -> Int -> Vector a
+-- col m@(Mat _ c _) ix = Vec c $ go m 0 ix
+--     where
+--         {-@ go :: m:NEMatrix a -> {rowx:Nat | mRow m >= rowx} -> {colx : Nat | mCol m > colx } -> [a] @-}
+--         go :: Matrix a -> Int -> Int -> [a]
+--         go m rowx colx = if (rowx >= mRow m) then [] else ind m rowx colx : go m (rowx + 1) colx
 
 
 impossible = undefined
 
-{-@ vmul :: v1:Vector Int -> v2:Vector Int -> Int @-}
+{-@ vmul :: v1:Vector Int -> {v2:Vector Int | mLen v1 == mLen v2} -> Int @-}
 vmul :: Vector Int -> Vector Int -> Int
-vmul (Vec _ []) (Vec _ []) = 0
+vmul (Vec 0 _) (Vec 0 _) = 0
 vmul (Vec _ []) _ = impossible
 vmul _ (Vec _ []) = impossible
 vmul (Vec n (x : xs)) ( Vec m (y : ys)) = x * y + (Vec (n-1) xs `vmul` Vec (m-1) ys)
 
+{-@ vplus :: v1:Vector Int -> {v2:Vector Int | mLen v1 == mLen v2} -> {v3:Vector Int | mLen v3 == mLen v1}@-}
 vplus :: Vector Int -> Vector Int -> Vector Int
-vplus (Vec n v1) (Vec _ v2) = Vec n $ go v1 v2
+vplus (Vec n v1) (Vec m v2) = Vec n $ go v1 v2
   where
       go :: [Int] -> [Int] -> [Int]
       go (x:xs) (y:ys) = x + y : go xs ys
@@ -52,28 +60,31 @@ vplus (Vec n v1) (Vec _ v2) = Vec n $ go v1 v2
       go [] _ = impossible
       go _ [] = impossible
 
-(***) :: Matrix Int -> Matrix Int -> Matrix Int
-(***) (Mat h w (Vec _ rows)) m2@(Mat h2 w2 _) = Mat h w2 $ Vec h $ go rows m2
-  where
-      go :: [Vector Int] -> Matrix Int -> [Vector Int]
-      go (x:xs) m@(Mat h w ms) = Vec w (rowxcol x 0 m) : go xs m
-      go [] _ = []
+-- (***) :: Matrix Int -> Matrix Int -> Matrix Int
+-- (***) (Mat _ _ (Vec _ rows)) m2 = Mat h w2 $ Vec h $ go rows m2
+--   where
+--       go :: [Vector Int] -> Matrix Int -> [Vector Int]
+--       go (x:xs) m@(Mat r c ms) = Vec r (rowxcol x 0 m) : go xs m
+--       go [] _ = []
 
-      rowxcol :: Vector Int -> Int -> Matrix Int -> [Int]
-      rowxcol x ix m@(Mat h w v) = if w > ix
-            then x `vmul` col m ix : rowxcol x (ix + 1) m
-            else []
+--       prod i j k m1 m2 = (ind m1 i j) * (ind m2 j k)
 
+--       rowxcol :: Int -> Int -> Matrix Int -> Matrix Int -> [Int]
+--       rowxcol row col m1 m2 = prod row 0 col m1 m2 : []
+
+{-@ (+++) :: m1:Matrix Int -> {m2:Matrix Int | mRow m1 == mRow m2 && mCol m1 == mCol m2} -> {m3:Matrix Int | mRow m3 == mRow m2 && mCol m3 == mCol m2} @-}
 (+++) :: Matrix Int -> Matrix Int -> Matrix Int
-(+++) (Mat h w (Vec _ r1)) (Mat h2 w2 (Vec _ r2)) = Mat h w $ Vec h $ go r1 r2
+(+++) (Mat h1 w1 (Vec _ r1)) (Mat h2 w2 (Vec _ r2)) = Mat h1 w1 $ Vec h1 $ go r1 r2
   where
       go :: [Vector Int] -> [Vector Int] -> [Vector Int]
       go (x:xs) (y:ys) = (x `vplus` y) : go xs ys
       go _ [] = []
       go [] _ = []
 
+{-@ v1 :: VectorN _ 4 @-}
 v1 :: Vector Int
 v1 = Vec 4 [1, 2, 3, 4]
+{-@ v2 :: VectorN _ 4 @-}
 v2 :: Vector Int
 v2 = Vec 4 [5, 6, 7, 8]
 v3 :: Vector Int
@@ -81,23 +92,26 @@ v3 = v1 `vplus` v2
 v4 :: Int
 v4 = v1 `vmul` v2
 
+{-@ m1 :: MatrixN _ 2 3 @-}
 m1 :: Matrix Int
 m1 = Mat 2 3 $ Vec 2 [Vec 3 [1, 2, 3], Vec 3 [4, 5, 6]]
+{-@ m2 :: MatrixN _ 2 3 @-}
 m2 :: Matrix Int
 m2 = Mat 2 3 $ Vec 2 [Vec 3 [-1, -2, -3], Vec 3 [-4, -5, -6]]
 m3 :: Matrix Int
 m3 = m1 +++ m2
 
+{-@ m4 :: MatrixN _ 3 2 @-}
 m4 :: Matrix Int
 m4 = Mat 3 2 $ Vec 3 [Vec 2 [10, 20], Vec 2 [30, 40], Vec 2 [50, 60]]
-m5 :: Matrix Int
-m5 = m1 *** m4
+-- m5 :: Matrix Int
+-- m5 = m1 *** m4
 
 main :: IO ()
 main = do
     print v3
     print v4
-    print $ col m4 0
-    print $ col m4 1
+    -- print $ col m4 0
+    -- print $ col m4 1
     print m3
-    print m5
+    -- print m5
